f3e783a274e1ce3f3c14e7a8749a4d11e1bafd37
Masatomo Nakano <tomo@quipper.com>
2013-02-17 09:03:50 +0000

    implement google apps auth

--- lib/quipper/middleware/auth.rb
+++ lib/quipper/middleware/auth.rb
@@ -1,9 +1,35 @@
 require "quipper/middleware/auth/version"
+require 'omniauth/strategies/google_apps'
 
 module Quipper
   module Middleware
     module Auth
-      # Your code goes here...
+      class GoogleApps
+        def initialize(app)
+          @app = app
+        end
+
+        def call(env)
+          request = Rack::Request.new(env)
+          session = env['rack.session']
+
+          if request.path == '/auth/g/callback'
+            if auth = request.env['omniauth.auth']
+              session[:google_apps_user_id] = auth['info']['email']
+              return [302, {'Location' => '/'}, []]
+            else
+              redirect '/auth/failure'
+            end
+          end
+
+          if request.path == '/auth/failure'
+            return [401, {"Content-Type" => "text/html"}, "login failed"]
+          end
+
+          return [302, {'Location' => '/auth/g'}, []] if !session[:google_apps_user_id]
+          @app.call(env)
+        end
+      end
     end
   end
 end
