eb62df29fde0342d322d7e8dff228c333ea5146b
Masatomo Nakano <tomo@quipper.com>
2013-02-21 13:00:20 +0000

    added whitelist

--- lib/quipper/middleware/auth.rb
+++ lib/quipper/middleware/auth.rb
@@ -10,7 +10,7 @@ module Quipper
         end
 
         def call(env)
-          return @app.call(env) if @options[:whitelist] && @options[:whitelist].include?(HTTP_X_FORWARDED_FOR)
+          return @app.call(env) if @options[:whitelist] && @options[:whitelist].include?(env["HTTP_X_FORWARDED_FOR"])
 
           request = Rack::Request.new(env)
           session = env['rack.session']
