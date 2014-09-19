d96992caf114f9a305797047a4097292dff94135
Tomo Nakano <tomo@quipper.com>
2014-09-19 14:03:43 +0100

    transpeced

--- spec/quipper/middleware/auth_spec.rb
+++ spec/quipper/middleware/auth_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe Quipper::Middleware::Auth do
   it 'should have a version number' do
-    Quipper::Middleware::Auth::VERSION.should_not be_nil
-  end
-
-  it 'should do something useful' do
-    false.should be_true
+    expect(Quipper::Middleware::Auth::VERSION).not_to be_nil
   end
 end
